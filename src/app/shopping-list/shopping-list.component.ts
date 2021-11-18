88ee9e6249d3cac400bd3cdb1a7cb6141890199c
Luca <l.pelissero.1360@vallauri.edu>
2021-11-18 11:14:40 +0100

    Recipe and Ingredient components layed on

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IngredientModel } from '../Models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingerdients:IngredientModel[]=[
+    new IngredientModel("Pasta", 500),
+    new IngredientModel("Pomodoro", 2)
+  ]
 
   constructor() { }
 
